9c2fd26a8cca8f4beb16fcfaaf74a1652cb402d1
GEO\lieu.hong.thai <lieu.hong.thai@geonet.co.jp>
2022-03-16 16:15:15 +0700

    update code

--- clientv2/src/pages/users/data/column.js
+++ clientv2/src/pages/users/data/column.js
@@ -1,51 +1,59 @@
-import { Tag, Space } from 'antd';
+import { Tag, Space, Button } from "antd";
 
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    render: text => <p>{text}</p>,
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-  {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: tags => (
-      <>
-        {tags.map(tag => {
-          let color = tag.length > 5 ? 'geekblue' : 'green';
-          if (tag === 'loser') {
-            color = 'volcano';
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>
-    ),
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (text, record) => (
-      <Space size="middle">
-        <p>Invite {record.name}</p>
-        <p>Delete</p>
-      </Space>
-    ),
-  },
-];
+const columns = (DeleteRow) =>{
+  return (
+    [
+      {
+        title: "Name",
+        dataIndex: "name",
+        key: "name",
+        render: (text) => <p>{text}</p>,
+      },
+      {
+        title: "Age",
+        dataIndex: "age",
+        key: "age",
+      },
+      {
+        title: "Address",
+        dataIndex: "address",
+        key: "address",
+      },
+      {
+        title: "Tags",
+        key: "tags",
+        dataIndex: "tags",
+        render: (tags) => (
+          <>
+            {tags.map((tag) => {
+              let color = tag.length > 5 ? "geekblue" : "green";
+              if (tag === "loser") {
+                color = "volcano";
+              }
+              return (
+                <Tag color={color} key={tag}>
+                  {tag.toUpperCase()}
+                </Tag>
+              );
+            })}
+          </>
+        ),
+      },
+      {
+        title: "Action",
+        key: "action",
+        render: (text, record, index) => (
+          <Space size="middle">
+            <p>Invite {record.name}</p>
+            {/* <Button onClick={()=>DeleteRow(index)}>Delete<Button/> */}
+            <Button onClick={()=>DeleteRow(index)}>Delete</Button>
+          </Space>
+        ),
+      },
+    ]
+  );
+
+} 
+
+
 export default columns;
--- clientv2/src/pages/users/table-user.js
+++ clientv2/src/pages/users/table-user.js
@@ -3,6 +3,8 @@ import { Table } from "antd";
 import columns from "./data/column";
 import dataRows from "./data/dataRows";
 const TableUser = () => {
+
+  const [rows,setRows] = React.useState([]);
   const [pagination, setPagination] = React.useState({
     current: 1,
     pageSize: 2,
@@ -10,16 +12,33 @@ const TableUser = () => {
   });
   // const [loading,setLoading] = React.useState(false);
 
+  React.useEffect(()=>{
+    setRows(dataRows);
+  },[]);
+
+  React.useMemo(()=>{
+    setPagination(dataState=>({
+      ...dataState,
+      total: rows.length
+    }))
+  },[rows])
+
   function onChangeHandle(pagination, filters, sorter, extra) {
     console.log(12005, pagination, filters, sorter, extra);
     setPagination(pagination);
+  }
+  
+  const DeleteRow = (zIndex)=>{
+    console.log(12005, zIndex);
+    const filterRow = rows.filter((value, index, arr)=> index !== zIndex);
+    setRows(filterRow);
   }
 
   return (
     <div>
       <Table
-        columns={columns}
-        dataSource={dataRows}
+        columns={columns(DeleteRow)}
+        dataSource={rows}
         pagination={pagination}
         onChange={onChangeHandle}
         loading={false}
